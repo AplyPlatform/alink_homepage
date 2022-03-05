563375aa19705e116c65ee435a72065d20019ff7
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-03-05 20:07:00 +0900

    스크립트 오타 수정

--- cs/js/script.js
+++ cs/js/script.js
@@ -40,7 +40,7 @@
                 return xhr;
             }                                          
         }).done(function(data) {            
-            location.href = "https://arin.aply.biz/cs/index.html";
+            location.href = "https://arink.aply.biz/cs/index.html";
         });
     };      
     // trigger the read from the reader...
