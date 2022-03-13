38455d7ffc814d3af2d70710e649572512059a13
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-03-13 12:55:47 +0900

    cs 코멘트 영역 레이아웃 수정3

--- cs/js/viewer.js
+++ cs/js/viewer.js
@@ -151,10 +151,10 @@ function setCurrentContent() {
 function showContent(content) {
     $('#currentImage').attr("src", "https://duni.io/arink/cs/images/" + content.filename);
         
-    let date = new Date(content.timestamp).toISOString().split("T")[0];    
+    let date = new Date(content.datetime).toISOString().split("T")[0];    
     const time = new Date().toTimeString().split(" ")[0];    
 
-    $('#currentMemo').html(content.memo + "<br>" + date + ' ' + time);
+    $('#currentMemo').html(content.memo + "<br><font size=1>" + date + ' ' + time + "</font>");
     $('#commentArea').show();
 
     popLabel.text(content.memo);
--- cs/sw.js
+++ cs/sw.js
@@ -1,5 +1,5 @@
 
-let CACHE_NAME = 'arink-store-v1_20220313_19';
+let CACHE_NAME = 'arink-store-v1_20220313_20';
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll([
