6ad3b8c996457bdbf399ad5527e2929debaf2aa8
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-03-13 12:45:14 +0900

    cs 코멘트 영역 레이아웃 수정

--- cs/sw.js
+++ cs/sw.js
@@ -1,5 +1,5 @@
 
-let CACHE_NAME = 'arink-store-v1_20220313_16';
+let CACHE_NAME = 'arink-store-v1_20220313_18';
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll([
