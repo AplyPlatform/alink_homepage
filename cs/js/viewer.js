3b9186fba4cd7b339e77eb43a249d1203f133bf5
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-03-13 10:52:12 +0900

    cs : 페이지네이션 추가

--- cs/js/viewer.js
+++ cs/js/viewer.js
@@ -3,12 +3,12 @@ $(function() {
     initViewer();        
 });
 
-let isCommentAreaVisible = false;
-let currentContentId = -1;
-let currentContentImage = "";
-let currentMemo = "";
 let currentLat, currentLng, currentAlt;
+
+let isCommentAreaVisible = false;
+let currentContentLat, currentContentLng;
 let oldLat = -999, oldLng = -999, oldAlt = -999;
+let currentContentArrays = [];
 
 function initViewer() {    
     let user_token = getCookie("user_token");
@@ -92,6 +92,9 @@ const clickListener = function(ev, target) {
     currentMemo = ev.target.getAttribute('memo');
     currentContentId = ev.target.getAttribute('c_id');
     currentContentImage = ev.target.getAttribute('content_image');
+
+    currentContentLat = ev.target.getAttribute('d_lat');
+    currentContentLng = ev.target.getAttribute('d_lng');
     
     const el = ev.detail.intersection && ev.detail.intersection.object.el;
 
@@ -109,27 +112,78 @@ const clickListener = function(ev, target) {
 
         if (isCommentAreaVisible == true) {
             isCommentAreaVisible = false;
-            $('#commentArea').hide();
+            $('#commentArea').hide();            
         }
-
-        setTimeout(() => {
-            getReplyContent();
-        }, 0);
-
+        else {
+            setTimeout(() => {
+                setCurrentContent();
+            }, 0);
+        }
     }
 };
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function showComments(comments) {                
-    $('#currentImage').attr("src", "https://duni.io/arink/cs/images/" + currentContentImage);
-    $('#currentMemo').text(currentMemo);
+function setCurrentContent() {
+    let contentsArrays = currentContentArrays[currentContentLat][currentContentLng];
+    $('#pagination').twbsPagination({
+        totalPages: contentsArrays.length,
+        visiblePages: 3,
+        onPageClick: function (event, page) {
+            //$('#page-content').text('Page ' + page);
+            let content = contentsArrays[page - 1];
+            showContent(content);
+        }
+    });
+}
+
+function showContent(content) {
+    $('#currentImage').attr("src", "https://duni.io/arink/cs/images/" + content.filename);
+    $('#currentMemo').text(content.memo);
     $('#commentArea').show();
 
+    getComments(content.id);
+}
+
+
+function getComments(c_id) {    
+    showLoader();
+
+    let sns_id = getCookie("temp_sns_id");
+    let skind = getCookie("dev_kind");
+    let user_token = getCookie("user_token");
+    let client_id = getCookie("user_clientid");        
+
+    var fd = new FormData();
+    fd.append('form_kind', "comment");
+    fd.append('c_id', c_id);
+    fd.append('sns_id', sns_id);
+    fd.append('sns_kind', skind);
+    fd.append('user_token', user_token);
+    fd.append('client_id', client_id);
+
+    $.ajax({
+        type: 'POST',
+        url: 'https://duni.io/arink/cs/handler/handler.php',
+        data: fd,
+        cache: false,
+        processData: false,
+        contentType: false                                                    
+    }).done(function(data) {
+        showComments(data.data);
+        hideLoader();
+    }).fail(function()  {
+        alert("Sorry. Server unavailable. ");
+        hideLoader();
+    });
+}
+
+
+function showComments(comments) {    
     isCommentAreaVisible = true;
     $('#commentReplyArea').empty();
     if (!comments || comments.length <= 0) {        
@@ -189,44 +243,9 @@ function writeComment() {
         processData: false,
         contentType: false                                                    
     }).done(function(data) {
-        $("#commentInput").val("");
         setTimeout(() => {
             getReplyContent();
         }, 0);
-    }).fail(function()  {
-        alert("Sorry. Server unavailable. ");
-        hideLoader();
-    });
-}
-
-function getReplyContent() {
-    if (currentContentId < 0) return;
-
-    let sns_id = getCookie("temp_sns_id");
-    let skind = getCookie("dev_kind");
-    let user_token = getCookie("user_token");
-    let client_id = getCookie("user_clientid");    
-    showLoader();
-
-    var fd = new FormData();
-    fd.append('user_id', 1324);
-    fd.append('form_kind', "comment");
-    fd.append('c_id', currentContentId);
-    fd.append('sns_id', sns_id);
-    fd.append('sns_kind', skind);
-    fd.append('user_token', user_token);
-    fd.append('client_id', client_id);
-
-    $.ajax({
-        type: 'POST',
-        url: 'https://duni.io/arink/cs/handler/handler.php',
-        data: fd,
-        cache: false,
-        processData: false,
-        contentType: false                                                    
-    }).done(function(data) {
-        showComments(data.data);
-        hideLoader();
     }).fail(function()  {
         alert("Sorry. Server unavailable. ");
         hideLoader();
@@ -267,24 +286,28 @@ function dynamicLoadPlaces() {
     });
 };
 
-function renderPlaces(places) {    
-    if (!isSet(places)) {
+function renderPlaces(placesArray) {    
+    if (!isSet(placesArray)) {
         $("#topText").text("No signals are loaded.");
         return;
     }
 
-    if (places.length == 1) $("#topText").text(places.length + " signal is loaded.");
-    else $("#topText").text(places.length + " signals are loaded.");
+    if (placesArray.length == 1) $("#topText").text(placesArray.length + " signal is loaded.");
+    else $("#topText").text(placesArray.length + " signals are loaded.");
+
+    currentContentArrays = placesArray;
 
     let scene = document.querySelector('a-scene');
-    let assets = document.querySelector('a-assets');
+    placesArray.forEach((places) => {
+        d = places[0];
 
-    places.forEach((d) => {
         let latitude = d.lat;
         let longitude = d.lng;
         
         let objetBox = document.createElement('a-box');
-        objetBox.setAttribute('c_id', d.id);         
+        objetBox.setAttribute('c_id', d.id);
+        objetBox.setAttribute('d_lat', latitude);
+        objetBox.setAttribute('d_lng', longitude);
         objetBox.setAttribute('rotation', '0 45 0');
         objetBox.setAttribute('position', '0 -5 0');
         objetBox.setAttribute('scale', '4.5 4.5 4.5');
@@ -293,6 +316,8 @@ function renderPlaces(places) {
         // add place name
         let objet = document.createElement('a-entity');
         objet.setAttribute('c_id', d.id);
+        objet.setAttribute('d_lat', latitude);
+        objet.setAttribute('d_lng', longitude);
         objet.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);                
         objet.setAttribute('scale', '0.2 0.2 0.2');
         objet.setAttribute('gltf-model', '/cs/assets/dog.glb');
@@ -305,12 +330,12 @@ function renderPlaces(places) {
         objet.setAttribute("smooth", "10");
         objet.setAttribute("smoothCount", "0.01");
         objet.setAttribute("smoothThreshold", "5");
-         
+            
         objet.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded', { detail: { component: this.el }}));
         });
 
         objet.appendChild(objetBox);
-        scene.appendChild(objet);        
-    });    
+        scene.appendChild(objet);
+    });        
 }
