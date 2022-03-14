ce970c1ead08aaf7f2d9a49b45bd71a5df7acaef
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-03-14 18:11:36 +0900

    오브젝트가 항상 한면을 보이게끔 개선중

--- cs/sw.js
+++ cs/sw.js
@@ -1,5 +1,5 @@
 
-let CACHE_NAME = 'arink-store-v1_20220314_04';
+let CACHE_NAME = 'arink-store-v1_20220314_05';
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll([
