32775579f16f8198d03f6a98f14c90c9b4224b09
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-03-15 00:59:23 +0900

    로드된 항목 수 표시 오류 수정2

--- cs/sw.js
+++ cs/sw.js
@@ -1,5 +1,5 @@
 
-let CACHE_NAME = 'arink-store-v1_20220315_07';
+let CACHE_NAME = 'arink-store-v1_20220315_08';
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll([
