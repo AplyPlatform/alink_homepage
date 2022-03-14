9230d0004298f5c15dd83c637c4d406d9ab99f61
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-03-15 00:57:58 +0900

    이벤트 오류 생기는 문제 수정중2

--- cs/sw.js
+++ cs/sw.js
@@ -1,5 +1,5 @@
 
-let CACHE_NAME = 'arink-store-v1_20220315_06';
+let CACHE_NAME = 'arink-store-v1_20220315_07';
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll([
