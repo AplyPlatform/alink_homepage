cac24d27c0b20b7d15670518e0bca6f627768950
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-03-16 15:23:53 +0900

    보더 라운더

--- cs/sw.js
+++ cs/sw.js
@@ -1,5 +1,5 @@
 
-let CACHE_NAME = 'arink-store-v1_20220315_52';
+let CACHE_NAME = 'arink-store-v1_20220316_3';
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll([
