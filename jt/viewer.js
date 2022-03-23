3320e97c8cfdc5ff29f4adde88843b0385e60d45
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-03-23 20:15:22 +0900

    제주토끼 전용 페이지 추가

---
+++ jt/viewer.js
@@ -0,0 +1,18 @@
+
+$(function(){ 
+  updateMindSet();
+
+  function updateMindSet() {
+    let mindStr = '<div><a-scene mindar-image="imageTargetSrc: jt.mind;"';
+
+    let bodyStr = mindStr
+                  + ' color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false"><a-assets></a-assets>'
+                  + '<a-camera position="0 0 0" look-controls="enabled: false"></a-camera>'
+                  + '<a-entity mindar-image-target="targetIndex: 0">'
+                  + '<a-plane geometry="primitive:plane;width:2;height:2;" material="shader:gif;src:url(./jt.gif);opacity:0.8"></a-plane>'
+                  + '</a-entity></a-scene></div>';
+    bodyStr += '<div id="flipButton"><font size="3" color="white">© 2022 <a href="https://aply.biz" target="_blank"><img src="https://aply.biz/assets/images/logo_white.png" border="0" width="45px"></a></font></div>';
+
+    $('body').html(bodyStr);    
+  }
+});    
