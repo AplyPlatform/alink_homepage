1d455e24587cf1bc3197e5da7bedbc3c867bad33
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-03-31 22:06:40 +0900

    필터 값 수정

--- an/js/3dviewer.js
+++ an/js/3dviewer.js
@@ -1,14 +1,14 @@
  $(function(){
   updateMindSet();  
   function updateMindSet() {
-    let mindStr = '<div><a-scene mindar-image="imageTargetSrc: ./assets/an.mind; filterMinCF:0.7; filterBeta: 5; warmupTolerance: 1; missTolerance: 1;';    
+    let mindStr = '<div><a-scene mindar-image="imageTargetSrc: ./assets/an.mind; filterMinCF:0.0001; filterBeta: 0.1;';    
 
     let bodyStr = mindStr
                   + 'showStats: false; autoStart: false; maxTrack: 5" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">'              
-                  + '<a-assets><a-asset-item id="raccoonModel" src="./assets/nw.glb"></a-asset-item></a-assets>'
+                  + '<a-assets><a-asset-item id="raccoonModel" src="./assets/an.glb"></a-asset-item></a-assets>'
                   + '<a-camera position="0 0 0" look-controls="enabled: false"></a-camera>'
                   + '<a-entity id="target-objet" mindar-image-target="targetIndex: 0">'
-                  + ' <a-gltf-model rotation="90 0 0" position="0 0 0" scale="0.04 0.04 0.04" src="#raccoonModel" animation-mixer></a-gltf-model>'
+                  + ' <a-gltf-model rotation="90 0 0" position="0 0 0" scale="0.8 0.8 0.8" src="#raccoonModel" animation-mixer></a-gltf-model>'
                   + '</a-entity></a-scene></div>';                  
     
     bodyStr += '<div id="flipButton"><font size="3" color="white">© 2022 <a href="https://aply.biz" target="_blank"><img src="https://aply.biz/assets/images/logo_white.png" border="0" width="45px"></a></font></div>';
