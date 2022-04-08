8d8636dd92921fc4ec075063c395c6e35410027e
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-04-08 21:11:12 +0900

    oy 오류 수정

--- oy/js/viewer.js
+++ oy/js/viewer.js
@@ -1,39 +1,16 @@
 
 $(function(){ 
-  updateMindSet();
-
-  function updateMindSet() {    
-    const portfolio = document.querySelector("#aplane");
-    portfolio.addEventListener('click', function (evt) {
-      window.open("https://www.oliveyoung.co.kr/store/goods/getGoodsDetail.do?goodsNo=A000000159707&dispCatNo=90000010001&trackingCd=Home_Planshop3");
-    });
-
-    const showPortfolio = () => {
-      const portfolio = document.querySelector("#portfolio-panel");
-      const paintandquestPreviewButton = document.querySelector("#paintandquest-preview-button");
-
-      let y = 0;
-      portfolio.setAttribute("visible", true);
-
-      const id = setInterval(() => {
-        y += 0.008;
-        if (y >= 0.6) {
-          clearInterval(id);          
-          paintandquestPreviewButton.addEventListener('click', () => {
-            paintandquestPreviewButton.setAttribute("visible", false);            
-            document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-mp4");
-            document.querySelector("#paintandquest-video-mp4").play();            
-          });
-        }
-        portfolio.setAttribute("position", "0 " + y + " -0.01");
-      }, 10);
-    }
-
+  updateMindSet();  
+  function updateMindSet() {
     AFRAME.registerComponent('mytarget', {
       init: function () {
         this.el.addEventListener('targetFound', event => {  
             setTimeout(() => {
-              showPortfolio();
+              showPortfolio(() => {
+                setTimeout(() => {
+                  showInfo();
+                }, 300);
+              });
             }, 300);          
         });
         this.el.addEventListener('targetLost', event => {
@@ -41,5 +18,41 @@ $(function(){
         });
       }
     });    
+  }
+
+  const setButtons = () => {
+    const couponButton = document.querySelector("#coupon-button");   
+    setTimeout(() => {
+      couponButton.setAttribute("visible", true);
+    }, 600);
+
+    couponButton.addEventListener('click', function (evt) {
+      window.open("https://www.oliveyoung.co.kr/store/goods/getGoodsDetail.do?goodsNo=A000000159707&dispCatNo=90000010001&trackingCd=Home_Planshop3");
+    });    
+  }
+
+  const showPortfolio = (done) => {
+    const portfolio = document.querySelector("#portfolio-panel");
+    const paintandquestPreviewButton = document.querySelector("#paintandquest-preview-button");
+
+    let y = 0;
+    portfolio.setAttribute("visible", true);
+
+    const id = setInterval(() => {
+      y += 0.008;
+      if (y >= 0.6) {
+        clearInterval(id);          
+        paintandquestPreviewButton.addEventListener('click', () => {
+          paintandquestPreviewButton.setAttribute("visible", false);            
+          document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-mp4");
+          document.querySelector("#paintandquest-video-mp4").play();            
+        });
+
+        setTimeout(() => {
+          done();
+        }, 500);
+      }
+      portfolio.setAttribute("position", "0 " + y + " -0.01");
+    }, 10);
   }
 });    
