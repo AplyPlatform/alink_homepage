30a20b01a3d53203e1fe725ed80726bf5b0590be
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-06-13 13:21:36 +0900

    j-meister : 불필요한 효과 더 삭제

--- aply/j-meister/js/viewer.js
+++ aply/j-meister/js/viewer.js
@@ -5,7 +5,7 @@ $(function() {
     document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-mp4");
     
     paintandquestPreviewButton.addEventListener('click', function (evt) {
-      paintandquestPreviewButton.setAttribute("visible", false);            
+      paintandquestPreviewButton.setAttribute("visible", false);
       document.querySelector("#paintandquest-video-mp4").play();
     });
   }
@@ -15,36 +15,20 @@ $(function() {
     portfolio.setAttribute("visible", true);
   }
 
-  function createSnow() {
-    const snow = document.createElement('i');
-    snow.classList.add('fas');
-    snow.classList.add('fa-snowflake');
-    snow.style.left = Math.random() * window.innerWidth + 'px';
-    snow.style.animationDirection = Math.random() * 3 + 2 + 's';
-    snow.style.opacity = Math.random();
-    snow.style.fontSize = Math.random() * 10 + 10 + 'px';
-
-    const snowlayer = document.querySelector("#snowlayer");      
-    snowlayer.appendChild(snow);
-  
-    setTimeout(() => {
-        snow.remove();
-    }, 5000);
-  }
-
   function updateMindSet() {
     const sceneEl = document.querySelector('a-scene');
     const cardTarget = document.querySelector("#guidecard");
     setButtons();
 
     sceneEl.addEventListener('targetFound', event => {                            
-      cardTarget.setAttribute("visible", false);
-      setInterval(createSnow, 100);
+      cardTarget.setAttribute("visible", false);      
       showPortfolio();
     });
       
-    sceneEl.addEventListener('targetLost', event => {      
+    sceneEl.addEventListener('targetLost', event => {            
+      paintandquestPreviewButton.setAttribute("visible", true);
       cardTarget.setAttribute("visible", true);
+      document.querySelector("#paintandquest-video-mp4").stop();
     });    
   }
 
