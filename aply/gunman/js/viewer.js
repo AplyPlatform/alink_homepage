8031c6f34003d0a5b6dc8594ce02e439697dacac
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-07-04 17:09:16 +0900

    gunman 프로필에 드론 추가

--- aply/gunman/js/viewer.js
+++ aply/gunman/js/viewer.js
@@ -13,6 +13,16 @@ $(function() {
   const showPortfolio = () => {
     const portfolio = document.querySelector("#portfolio-panel");
     portfolio.setAttribute("visible", true);
+    
+    const avatar = document.querySelector("#avatar");
+    let z = -0.3;
+    const id = setInterval(() => {
+      z += 0.02;
+      if (z >= 1.5) {
+        clearInterval(id);
+      }
+      avatar.setAttribute("position", "0 0 " + z);
+    }, 10);    
   }
 
   function updateMindSet() {
