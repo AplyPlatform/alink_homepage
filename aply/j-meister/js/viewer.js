d495ef5b8aba1852bc0baaadc02e315c943e1747
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-07-23 17:29:51 +0900

    APLY 모든 멤버 대상 글문구 표시 기능 추가

--- aply/j-meister/js/viewer.js
+++ aply/j-meister/js/viewer.js
@@ -1,36 +1,4 @@
-
-$(function() {     
-  const setButtons = () => {    
-    const paintandquestPreviewButton = document.querySelector("#paintandquest-preview-button");
-    document.querySelector("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-mp4");
-    
-    paintandquestPreviewButton.addEventListener('click', function (evt) {
-      paintandquestPreviewButton.setAttribute("visible", false);
-      document.querySelector("#paintandquest-video-mp4").play();
-    });
-  }
-
-  const showPortfolio = () => {
-    const portfolio = document.querySelector("#portfolio-panel");
-    portfolio.setAttribute("visible", true);
-  }
-
-  function updateMindSet() {
-    const sceneEl = document.querySelector('a-scene');
-    const cardTarget = document.querySelector("#guidecard");
-    setButtons();
-
-    sceneEl.addEventListener('targetFound', event => {                            
-      cardTarget.setAttribute("visible", false);      
-      showPortfolio();
-    });
-      
-    sceneEl.addEventListener('targetLost', event => {            
-      paintandquestPreviewButton.setAttribute("visible", true);
-      cardTarget.setAttribute("visible", true);
-      document.querySelector("#paintandquest-video-mp4").stop();
-    });    
-  }
-
+$(function() {
   updateMindSet();
+  get_message("jmeister");
 });    
