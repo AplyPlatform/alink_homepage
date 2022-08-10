86c730aef46034787bc1a3ecb1de5f5935d5a680
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-08-10 21:53:36 +0900

    [wip] 포옹 전시회 초청장 페이지 개선중

--- eh/js/look.js
+++ eh/js/look.js
@@ -6,16 +6,17 @@ $(function() {
         return 'Bye';   
     };
     
-    updateMindSet();
-    get_message();    
+    //updateMindSet();
+    //get_message();    
   });  
 
-var currentPostId = -1;
 
 function updateMindSet() {    
     const setButtons = () => {            
         mSel("#paintandquest-video-link").setAttribute("src", "#paintandquest-video-mp4");
 
+        $("#area_comment_writer").hide();
+    
         mSel("#paintandquest-preview-button").addEventListener('click', function (evt) {
             mSel("#paintandquest-preview-button").setAttribute("visible", false);
             mSel("#paintandquest-video-mp4").play();
@@ -36,10 +37,6 @@ function updateMindSet() {
         mSel("#map_button").addEventListener('click', function (evt) {
             setMapPage();
         });
-
-        mSel("#like_button").addEventListener('click', function (evt) {
-            likeMessage();
-        });        
     };
 
     const showPortfolio = () => {
@@ -60,24 +57,16 @@ function updateMindSet() {
         mSel("#paintandquest-video-mp4").pause();
     }); 
     
-
-    if (getCookie("user_like") == "liked") {
-        mSel("#like_button").setAttribute("src", "#hearticon");        
-    }
-
     setFirstPage();    
 }
 
 function playSound(id) {
-    if (id == 1)
-        new Audio("./assets/ex/button.wav").play();
-    else
-        new Audio("./assets/ex/like.wav").play();
+    new Audio("./assets/ex/button.wav").play();
 }
 
 
 function setFirstPage() {
-    playSound(1);
+    playSound(0);
 
     mSel("#portfolio-item0").setAttribute("visible", false);
     mSel("#paintandquest-video-mp4").currentTime = 0;
@@ -85,8 +74,8 @@ function setFirstPage() {
 
     mSel("#main_image_area1").setAttribute("src", "#selfi_image");
     mSel("#main_image_area1").setAttribute("visible", true);
-    mSel("#main_image_area1").setAttribute("width", "0.62");
-    mSel("#main_image_area1").setAttribute("height", "0.76");
+    mSel("#main_image_area1").setAttribute("width", "0.5");
+    mSel("#main_image_area1").setAttribute("height", "0.7");
     mSel("#main_image_area1").setAttribute("position", "0 0.1 0");
 
     mSel("#main_image_area2").setAttribute("visible", false);
@@ -117,13 +106,13 @@ function setSecondPage() {
     mSel("#main_image_area3").setAttribute("visible", true);
     mSel("#main_image_area3").setAttribute("width", "0.6");
     mSel("#main_image_area3").setAttribute("height", "0.2");
-    mSel("#main_image_area3").setAttribute("position", "0 0.15 0.01");
+    mSel("#main_image_area3").setAttribute("position", "0 0.15 -0.1");
 
     mSel("#main_image_area4").setAttribute("src", "#desk2_image");
     mSel("#main_image_area4").setAttribute("visible", true);
     mSel("#main_image_area4").setAttribute("width", "0.6");
     mSel("#main_image_area4").setAttribute("height", "0.2");
-    mSel("#main_image_area4").setAttribute("position", "0 -0.06 0.03");
+    mSel("#main_image_area4").setAttribute("position", "0 -0.06 0.2");
 
 }
 
@@ -175,41 +164,6 @@ function writeMessage()  {
     });
 }
 
-function likeMessage()  {
-
-    if (getCookie("user_like") == "liked") {
-        setCookie("user_like", "", 1);
-        likeCancelMessage();
-        mSel("#like_button").setAttribute("src", "#hearticon_before");
-        return;
-    }
-
-    playSound(0);
-    setCookie("user_like", "liked", 1);
-    mSel("#like_button").setAttribute("src", "#hearticon");
- 
-    var formData = new FormData();
-    formData.append("form_kind", "like_action");
-    formData.append("user", "eh");
-    formData.append("docu_srl", currentPostId);    
-
-    ajaxRequest(formData, function (r) {
-    }, function (r,s,e) {                
-    });
-}
-
-function likeCancelMessage()  {
- 
-    var formData = new FormData();
-    formData.append("form_kind", "like_action_cancel");
-    formData.append("user", "eh");
-    formData.append("docu_srl", currentPostId);    
-
-    ajaxRequest(formData, function (r) {        
-    }, function (r,s,e) {        
-    });
-}
-
 
 function get_message() {
     var formData = new FormData();
@@ -221,8 +175,6 @@ function get_message() {
       const comment_a2 = document.querySelector("#comment_a2");
       comment_a1.setAttribute("value", r[0].title);
       comment_a2.setAttribute("value", r[0].content);
-      currentPostId = r[0].docu_srl;
-
     }, function (r,s,e) {
   
     });
@@ -252,22 +204,4 @@ function ajaxRequest(data, callback, errorcallback) {
             errorcallback(request, status, error);
         }
     });
-}
-
-function delCoockie(cName) {
-    document.cookie = name + "= " + "; expires=" + date.toUTCString() + "; path=/;";
-}
-
-function setCookie(cName, cValue, cDay) {
-    var date = new Date();
-    date.setTime(date.getTime() + cDay * 60 * 60 * 24 * 1000);
-    document.cookie = cName + '=' + cValue + '; expires=' + date.toUTCString() + '; path=/;';
-}
-
-function getCookie(cName) {
-    let matches = document.cookie.match(new RegExp(
-        "(?:^|; )" + cName.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-
-    return matches ? matches[1] : null;
 }
