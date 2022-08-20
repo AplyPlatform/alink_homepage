2133fbba973d298892f79c99c7af8614354a7f71
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2022-08-20 11:32:52 +0900

    안내카드 레이아웃 개선

--- eh/js/guidecard.js
+++ eh/js/guidecard.js
@@ -1,5 +1,3 @@
-'use strict';
-
 $(function() {
     
     document.title = "포옹전 작품 가이드 | APLX";
@@ -8,21 +6,14 @@ $(function() {
         eventOC_OUT();
         return undefined;   
     };
-    updateMindSet();    
-
-    setTimeout(function(){
-        // This hides the address bar:
-        window.scrollTo(0, 1);
-    }, 0);
+    updateMindSet();
 });  
 
 var targetFileId = 470;
 var currentPostId = -1;
 var jobIds = [496, 517];
-var commentArrayData = [];
 var pageContents = [];
 var currrentPage = 0;
-var currentCommentCount = 0;
 var hideTimeout = -1, showTimeout = -1;
 
 var intervalHandle = null;
@@ -47,27 +38,6 @@ function updateMindSet() {
             playSound(1);
             return;
         });
-
-        mSel("#like_button").addEventListener('click', function (evt) {
-            likeMessage();
-        });
-
-        mSel("#write_comment_button").addEventListener('click', function (evt) {
-            mSel("#bottom_border").style.display = 'none';
-            mSel("#top_border").style.display = 'none';
-            $("#area_comment_writer").show();
-        });
-
-        mSel("#closeButton").addEventListener('click', function(evt) {
-            mSel("#bottom_border").style.display = 'block';
-            mSel("#top_border").style.display = 'block';
-            $("#area_comment_writer").hide();
-        });
-
-        mSel("#replyButton").addEventListener('click', function(evt) {            
-            writeMessage();            
-        });
-
     };
 
     const showPortfolio = () => {
@@ -83,13 +53,12 @@ function updateMindSet() {
     });
         
     sceneEl.addEventListener('targetLost', event => {
-        //mSel("#bottom_border").style.display = 'none';
+        mSel("#bottom_border").style.display = 'none';
         pauseScroll();
     });
 
-    $("#oScroll").height(window.innerHeight - 238);
+    $("#oScroll").height(window.innerHeight - 180);
     setButtons();
-    showComment();
     get_messages();
 }
 
@@ -124,7 +93,6 @@ function setPageAssets(docu_id) {
     currentPostId = docu_id;
 
     var curContent = pageContents[currentPostId];    
-    commentArrayData = curContent.comments;
     
     mSel("#over_image").setAttribute("src", curContent.filename);
 
@@ -132,10 +100,8 @@ function setPageAssets(docu_id) {
     mSel("#over_image").setAttribute("height", ratio + 0.02);
     mSel("#over_image").setAttribute("width", 1.02);
 
-    mSel("#comment_a1").innerHTML = curContent.title;
-    mSel("#comment_a2").innerHTML = curContent.content;    
-
-    setLikeButtonStatus(currentPostId);    
+    //mSel("#comment_a1").innerHTML = curContent.title;
+    mSel("#comment_a2").innerHTML = curContent.content;
 }
 
 
@@ -156,55 +122,6 @@ function setSecondPage() {
     startHistoryScroll();
 }
 
-function setLikeButtonStatus(docu_id) {
-    if (getCookie("user_like_" + docu_id) == "liked") {
-        mSel("#like_button").setAttribute("src", ""); 
-        mSel("#like_button").setAttribute("src", "./assets/icon_heart.png");
-        return;
-    }    
-  
-    mSel("#like_button").setAttribute("src", ""); 
-    mSel("#like_button").setAttribute("src", "./assets/ex/icon_heart1@3x.png");
-}
-
-function likeMessage()  {
-  if (getCookie("user_like_" + currentPostId) == "liked") {
-      setCookie("user_like_" + currentPostId, "", 1);
-      likeCancelMessage(currentPostId);
-      mSel("#like_button").setAttribute("src", ""); 
-      mSel("#like_button").setAttribute("src", "./assets/ex/icon_heart1@3x.png"); 
-      return;
-  }
-
-  playSound(0);
-  setCookie("user_like_" + currentPostId, "liked", 1);
-  mSel("#like_button").setAttribute("src", "");
-  mSel("#like_button").setAttribute("src", "./assets/icon_heart.png");
-
-  var formData = new FormData();
-  formData.append("form_kind", "like_action");
-  formData.append("user", "eh");
-  formData.append("docu_srl", currentPostId);    
-
-  ajaxRequest(formData, function (r) {    
-    }, function (r,s,e) {                
-  });
-}
-
-function likeCancelMessage(docu_id)  {
-
-  var formData = new FormData();
-  formData.append("form_kind", "like_action_cancel");
-  formData.append("user", "eh");
-  formData.append("docu_srl", docu_id);    
-
-  ajaxRequest(formData, function (r) {        
-    }, function (r,s,e) {        
-  });
-
-}
-
-
 var currentJobIndex = 0;
 function get_messages() {
 
@@ -220,14 +137,7 @@ function get_messages() {
   ajaxRequest(formData, function (r) {        
     if (isSet(r) && r.length > 0) {                   
         
-        var commentArray = [];
-        if ("comments" in r[0] && r[0].comments.length > 0) {            
-            r[0].comments.forEach(function (v, i, arr) {                
-                commentArray.push(v.content + " | " + v.name);                
-            });            
-        }
-
-        pageContents[docu_id] = {title : r[0].title, content : r[0].content, filename : r[0].file, comments : commentArray, width:r[0].width, height:r[0].height};
+        pageContents[docu_id] = {title : r[0].title, content : r[0].content, filename : r[0].file, width:r[0].width, height:r[0].height};
 
         if (currentJobIndex < jobIds.length) {
             get_messages();            
@@ -243,78 +153,7 @@ function get_messages() {
 }
 
 
-function showComment() {    
-    if (commentArrayData.length <= 0) {
-        hideTimeout = window.setTimeout(hideComment, 1000);
-        return;
-    }
-
-    if (currentCommentCount >= commentArrayData.length) currentCommentCount = 0;    
-    var textToShow = commentArrayData[currentCommentCount];
-    currentCommentCount++;
-
-    if (textToShow.length > 28) {
-        textToShow = textToShow.substring(0, 28);
-        textToShow += " ...";
-    }
-
-    mSel("#comment_area").innerHTML = textToShow + " <img src='./assets/icon_heart.png' width='8px'>";
-    mSel("#comment_area").classList.remove("fade");
-    
-    hideTimeout = window.setTimeout(hideComment, 3500);
-}
-
-function hideComment() {    
-    mSel("#comment_area").classList.add("fade");
-    showTimeout = window.setTimeout(showComment, 1000);
-}
-
-
-function writeMessage()  {
-    var comment = $("#commentInput").val();
-    var name = $("#nameInput").val();
-
-    if (!isSet(comment) && comment == "") {
-        alert("댓글을 작성해 주세요 :)");
-        return;
-    }
-
-    if (comment.length > 25) {
-        alert("댓글의 길이가 너무 길어요. 25자 이내로 굵고, 짧게 작성 부탁드려요 :)");
-        return;
-    }
-
-    if (!isSet(name) && name == "") {
-        alert("이름을 작성해 주세요 :)");
-        return;
-    }
-
-    if (name.length > 10) {
-        alert("이름의 길이가 너무 길어요. 10자 이내로 작성 부탁드려요 :)");
-        return;
-    }
- 
-    var formData = new FormData();
-    formData.append("form_kind", "write_comment");
-    formData.append("user", 'eh');
-    formData.append("docu_srl", currentPostId);
-    formData.append("comment", comment);
-    formData.append("name", name);
-
-    ajaxRequest(formData, function (r) {        
-        commentArrayData.unshift(comment + " | " + name);        
-        $("#area_comment_writer").hide();
-        mSel("#bottom_border").style.display = 'block';
-        mSel("#top_border").style.display = 'block';
-        alert("감사합니다. 즐거운 관람 되세요!");
-    }, function (r,s,e) {
-        $("#area_comment_writer").hide();
-        mSel("#bottom_border").style.display = 'block';
-        mSel("#top_border").style.display = 'block';
-        alert("작성 실패! - 잠시 후 다시 시도해 주세요.")
-    });
-}
-
+var vScroll;
 function pauseScroll() {
     if(isStop == true) {        
         return;
@@ -342,7 +181,7 @@ function scroll(oid, iid) {
         this.n--;
     }
 }
-var vScroll;
+
 function startHistoryScroll() {
     isStop = false;
     vScroll = new scroll("oScroll", "scroll");
